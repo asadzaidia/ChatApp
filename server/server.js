3c5953d889e9b72e9a316a99aef3fa1664b8d5bb
asadzaidia <asadzaidi625@gmail.com>
2017-10-19 08:55:24 +0500

    Add connect and disconnet

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path=require('path');
 const express=require('express');
+const http=require('http');
+const socketio=require('socket.io');
 const publicPath=path.join(__dirname,'../public'); //its better to use it because of its feasability
 // console.log(__dirname+'/../public');
 const port=process.env.PORT || 3000;
 console.log(publicPath);
 
 var app=express();
+var server=http.createServer(app);//making because chat use http
+var io=socketio(server);//Integrating server to io
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{ // socket app is a parameter from index page io is a connection on is a method
+console.log('New User Conneted');
+socket.on('disconnect',()=>{
+  console.log('User was disconnected');
+});
+});
+
+
+
+server.listen(port,()=>{ //http connection because chat using that protocol
   console.log(`Server is up on ${port}`);
 });
