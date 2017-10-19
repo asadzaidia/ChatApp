3e8c7939bb5ff891d45210520ea7ca226c180c72
asadzaidia <asadzaidi625@gmail.com>
2017-10-19 12:14:46 +0500

    utils for generating message and tests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path=require('path');
 const express=require('express');
 const http=require('http');
 const socketio=require('socket.io');
+const {generateMessage}=require('./utils/message');
 const publicPath=path.join(__dirname,'../public'); //its better to use it because of its feasability
 // console.log(__dirname+'/../public');
 const port=process.env.PORT || 3000;
@@ -20,25 +21,14 @@ console.log('New User Conneted');
 //   msg:'Hello sending from a server side to a client',
 //   createAt:1234
 // });//newEmail is the defined in index custom event
-socket.emit('NewMessage',{ //greetings
-  from:'Admin',
-  text:'Welcome to the Chat App',
-  createdAt:new Date().getTime()
-});
-socket.broadcast.emit('NewMessage',{
-  from:'Admin',
-  text:'New User joined',
-  createdAt:new Date().getTime()
-});
+socket.emit('NewMessage',generateMessage('Admin','Welcome to the Chat app'));
+socket.broadcast.emit('NewMessage',generateMessage('Admin','New User Has Joined this'));
 
 
-socket.on('createMessage',(message)=>{ //receiving from clint side
+socket.on('createMessage',(message)=>{
   console.log('createMessage',message);
-  io.emit('NewMessage',{
-    from:message.from,
-    text:message.text,
-    createdAt:new Date().getTime()
-  });
+  io.emit('NewMessage',generateMessage(message.from,message.text));
+
   // socket.broadcast.emit('NewMessage',{ //broadcast use when sending to everyone except yourself
   //   from:message.from,
   //   text:message.text,
