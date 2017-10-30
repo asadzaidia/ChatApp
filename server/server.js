5a6ad9d3f31126bf8d64ff2076a6035e3fe702a9
asadzaidia <asadzaidi625@gmail.com>
2017-10-30 16:25:11 +0500

    Private Chat functionality

--- server/server.js
+++ server/server.js
@@ -39,8 +39,11 @@ callback();
 });
 
 socket.on('createMessage',(message,callback)=>{ //callback is a function return from index emit
-  console.log('createMessage',message);
-  io.emit('NewMessage',generateMessage(message.from,message.text));
+  var user =users.getUser(socket.id);
+  if(user && isRealString(message.text)){
+    io.to(user.room).emit('NewMessage',generateMessage(user.name,message.text));
+  }
+
   callback('');
   // socket.broadcast.emit('NewMessage',{ //broadcast use when sending to everyone except yourself
   //   from:message.from,
@@ -50,7 +53,11 @@ socket.on('createMessage',(message,callback)=>{ //callback is a function return 
 });
 
 socket.on('createLocationMessage',(coords)=>{
-io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+  var user=users.getUser(socket.id);
+  if(user){
+      io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+  }
+
 });
 
 socket.on('disconnect',()=>{
